058d2b3d5b39e747c9b6eebe2a930d5302d92478
Ricardo6647 <ziko.miguel@live.de>
2026-04-26 23:52:11 +0200

    Update admin.routes.js

--- admin.routes.js
+++ admin.routes.js
@@ -1,23 +1,36 @@
 const router = require('express').Router();
 const adminController = require('./admin.controller');
 
-// USERS
+/* -------------------- USERS -------------------- */
+// Liste aller User (Admin only)
 router.get('/users', adminController.getUsers);
 
-// PRODUCTS
+/* -------------------- PRODUCTS -------------------- */
+// Alle Produkte abrufen
 router.get('/products', adminController.getProducts);
+// Produkt hinzufügen
+router.post('/products', adminController.addProduct);
+// Produkt löschen
+router.delete('/products/:id', adminController.deleteProduct);
 
-// SHOPS
+/* -------------------- SHOPS -------------------- */
+// Alle Shops abrufen
 router.get('/shops', adminController.getShops);
 
-// CATEGORIES
+/* -------------------- CATEGORIES -------------------- */
+// Kategorien abrufen
 router.get('/categories', adminController.getCategories);
+// Kategorie hinzufügen
 router.post('/categories', adminController.addCategory);
+// Kategorie löschen
 router.delete('/categories/:id', adminController.deleteCategory);
 
-// BANNERS
+/* -------------------- BANNERS -------------------- */
+// Banner abrufen
 router.get('/banners', adminController.getBanners);
+// Banner hinzufügen
 router.post('/banners', adminController.addBanner);
+// Banner löschen
 router.delete('/banners/:id', adminController.deleteBanner);
 
 module.exports = router;
